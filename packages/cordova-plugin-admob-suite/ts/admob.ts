40a5a27654f0fc91b7cfc35392d0a3bcdd1561c2
Ratson <github@ratson.name>
2018-06-20 22:57:28 +0800

    refactor(cordova-ios): add execAsync helper

--- packages/cordova-plugin-admob-suite/ts/admob.ts
+++ packages/cordova-plugin-admob-suite/ts/admob.ts
@@ -1,1 +1,18 @@
-export = {}
+
+import { exec } from 'cordova'
+
+const enum NativeActions {
+  Service = 'AdMob',
+  show = 'show'
+}
+
+function execAsync(action: NativeActions, args?: string[]) {
+  return new Promise((resolve, reject) => {
+    exec(resolve, reject, NativeActions.Service, action, args)
+  })
+}
+
+class AdMob {
+}
+
+export = new AdMob()
