af5f0ff1cf4c7a806971bb022f63976af6b3d147
Ratson <github@ratson.name>
2018-06-25 22:41:30 +0800

    feat(cordova-android): add plugin class

---
+++ packages/cordova-plugin-admob-suite/src/android/AdMobPlugin.java
@@ -0,0 +1,14 @@
+package admob.suite;
+
+import org.apache.cordova.CordovaPlugin;
+
+public class AdMobPlugin extends CordovaPlugin {
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+    }
+
+    @Override
+    public boolean execute(String action, JSONArray inputs, CallbackContext callbackContext) throws JSONException {
+    }
+}
