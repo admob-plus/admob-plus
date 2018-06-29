0fed2b2396b56750cc46ac38d7411a2f9619a39a
Ratson <github@ratson.name>
2018-06-29 23:30:11 +0800

    chore(example): add ionic-admob to ionic-angular example

--- examples/ionic-angular/src/app/app.component.ts
+++ examples/ionic-angular/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { Platform } from "ionic-angular";
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
+import { AdMob } from "ionic-admob";
 
 import { TabsPage } from "../pages/tabs/tabs";
 
@@ -14,13 +15,16 @@ export class MyApp {
   constructor(
     platform: Platform,
     statusBar: StatusBar,
-    splashScreen: SplashScreen
+    splashScreen: SplashScreen,
+    admob: AdMob
   ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      admob.interstitial.prepare();
     });
   }
 }
--- examples/ionic-angular/src/app/app.module.ts
+++ examples/ionic-angular/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, ErrorHandler } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { IonicApp, IonicModule, IonicErrorHandler } from "ionic-angular";
+
 import { MyApp } from "./app.component";
 
 import { AboutPage } from "../pages/about/about";
@@ -10,6 +11,7 @@ import { TabsPage } from "../pages/tabs/tabs";
 
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
+import { AdMob } from "ionic-admob";
 
 @NgModule({
   declarations: [MyApp, AboutPage, ContactPage, HomePage, TabsPage],
@@ -17,6 +19,7 @@ import { SplashScreen } from "@ionic-native/splash-screen";
   bootstrap: [IonicApp],
   entryComponents: [MyApp, AboutPage, ContactPage, HomePage, TabsPage],
   providers: [
+    AdMob,
     StatusBar,
     SplashScreen,
     { provide: ErrorHandler, useClass: IonicErrorHandler }
