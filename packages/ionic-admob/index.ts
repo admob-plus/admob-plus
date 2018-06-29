4b051132339397b72328ef7545d3bf1b119d5e85
Ratson <github@ratson.name>
2018-06-29 23:18:13 +0800

    feat(ionic-admob): create package

---
+++ packages/ionic-admob/index.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Cordova, IonicNativePlugin, Plugin } from '@ionic-native/core';
+
+@Plugin({
+  plugin: 'cordova-plugin-admob-suite',
+  pluginName: 'AdMob',
+  pluginRef: 'admob.interstitial'
+})
+export class AdMobInterstitial {
+  @Cordova()
+  prepare(): Promise<any> {
+    return Promise.resolve();
+  }
+
+  @Cordova()
+  show(): Promise<any> {
+    return Promise.resolve();
+  }
+}
+
+@Plugin({
+  platforms: ['Android', 'iOS'],
+  plugin: 'cordova-plugin-admob-suite',
+  pluginName: 'AdMob',
+  pluginRef: 'admob',
+  repo: 'https://github.com/admob-suite/admob-suite'
+})
+@Injectable()
+export class AdMob extends IonicNativePlugin {
+  interstitial: AdMobInterstitial = new AdMobInterstitial();
+}
