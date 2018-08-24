db2484f44efd863c6b5dec9d1142141b01528bcc
Ratson <github@ratson.name>
2018-08-24 22:03:28 +0800

    feat(cordova): add browser platform support

---
+++ packages/cordova/src/browser/AdMobProxy.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const AdMob = {
+  banner_hide() {},
+  banner_show() {},
+  interstitial_load() {},
+  interstitial_show() {},
+  ready() {},
+  reward_video_load() {},
+  reward_video_show() {},
+  set_app_muted() {},
+  set_app_volume() {},
+}
+
+// eslint-disable-next-line node/no-missing-require
+require('cordova/exec/proxy').add('AdMob', AdMob)
