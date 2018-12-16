74f9ec54fb8c587c842b773fca11f1c06f69e84a
Ratson <github@ratson.name>
2018-12-17 01:12:27 +0800

    feat(cordova): allow multiple banners

--- packages/cordova/ts/banner.ts
+++ packages/cordova/ts/banner.ts
@@ -1,25 +1,25 @@
-import { AdSizeType, IBannerRequest } from '@admob-plus/core'
+import { AdSizeType, AdUnitIDOption, IBannerRequest } from '@admob-plus/core'
 
 import { AdBase, execAsync, NativeActions, TestIds } from './base'
-
-const AD_ID = 1
 
 export default class Banner extends AdBase {
   protected testIdForAndroid = TestIds.banner_android
   protected testIdForIOS = TestIds.banner_ios
 
   public show(opts: IBannerRequest) {
+    const adUnitID = this.resolveAdUnitID(opts.id)
     return execAsync(NativeActions.banner_show, [
       {
         size: AdSizeType.SMART_BANNER,
         ...opts,
-        adUnitID: this.resolveAdUnitID(opts.id),
-        id: AD_ID,
+        adUnitID,
+        id: this.state.getAdId(adUnitID),
       },
     ])
   }
 
-  public hide() {
-    return execAsync(NativeActions.banner_hide, [{ id: AD_ID }])
+  public hide(id: AdUnitIDOption) {
+    const adUnitID = this.resolveAdUnitID(id)
+    return execAsync(NativeActions.banner_hide, [{ id: adUnitID }])
   }
 }
