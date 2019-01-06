412e635f40298d9a8da909de7c7cbcca8e766f9b
Ratson <github@ratson.name>
2019-01-07 00:24:42 +0800

    docs: replace users broken image url

--- website/data/users.js
+++ website/data/users.js
@@ -2,17 +2,9 @@
 
 module.exports = [
   {
-    caption: 'Example App',
-    image:
-      'https://raw.githubusercontent.com/admob-plus/admob-plus/master/examples/basic/res/icon/android/icon-96-xhdpi.png',
-    infoLink:
-      'https://github.com/admob-plus/admob-plus/tree/master/examples/basic',
-  },
-  {
     caption: 'Eternity Pilot',
     image:
-      'http://b3l7.com/images/eternityPilot.png',
-    infoLink:
-      'http://eternitypilot.com',
+      'https://lh3.googleusercontent.com/8Crgn4XYSeg9iawdbpiCXU7OoRuqL_8PpJt8YGy_ywrtZjhFMcDVpSHfYaOBI4zANVA',
+    infoLink: 'https://eternitypilot.com',
   },
 ]
