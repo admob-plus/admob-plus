aa84fd425c491d8708e52e1cbe7da0a519f4d166
Naveed Ahmad <naveedahmada036@gmail.com>
2019-01-22 03:21:14 +0500

    added my game which are using admob-plus

--- website/data/users.js
+++ website/data/users.js
@@ -2,6 +2,24 @@
 
 module.exports = [
   {
+    caption: 'Jelly doods',
+    image:
+      'https://lh3.googleusercontent.com/oMqa6mkyEN8vef5raCBF2nRYzuvQEimMofBBbuCugt4pK0_ByrBYqv8Oti8dumf_Ftg=s360-rw',
+    infoLink: 'https://play.google.com/store/apps/details?id=com.gameslab.jelly_doods',
+  },
+  {
+    caption: 'Virus Attack',
+    image:
+      'https://lh3.googleusercontent.com/zuu2x6veZ6SuziM3Yj16J5B2Ry08gitbxIXYEc_zGvb-qduFrJ5AL0p6dA_rx8G0EA=s360-rw',
+    infoLink: 'https://play.google.com/store/apps/details?id=com.gameslab.virus_attack',
+  },
+  {
+    caption: 'Battleship war',
+    image:
+      'https://lh3.googleusercontent.com/YGfluW3W9nW4GB5Ioqt30nN6ZwGYvVLuxNTjLt6aATrtZDV-lfMB-WOo_4VyGjLHPGU=s360-rw',
+    infoLink: 'https://play.google.com/store/apps/details?id=com.gameslab.battleship_war',
+  },
+  {
     caption: 'Eternity Pilot',
     image:
       'https://lh3.googleusercontent.com/8Crgn4XYSeg9iawdbpiCXU7OoRuqL_8PpJt8YGy_ywrtZjhFMcDVpSHfYaOBI4zANVA',
