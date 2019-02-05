2d9e65939b633eeed107c8d43cc2f1e78f1c973a
Sander van Kasteel <info@sandervankasteel.nl>
2019-02-05 22:07:52 +0100

    Added Ludum as user of Admob Plus

--- website/data/users.js
+++ website/data/users.js
@@ -25,4 +25,10 @@ module.exports = [
       'https://lh3.googleusercontent.com/8Crgn4XYSeg9iawdbpiCXU7OoRuqL_8PpJt8YGy_ywrtZjhFMcDVpSHfYaOBI4zANVA',
     infoLink: 'https://eternitypilot.com',
   },
+  {
+    caption: 'Ludum',
+    image:
+      'https://ludum.app/assets/images/logo-sliced.png',
+    infoLink: 'https://ludum.app/',
+  },
 ]
