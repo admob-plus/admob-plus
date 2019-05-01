00a329932227a03902b296d826dd3958bfb3c496
Ratson <github@ratson.name>
2019-05-01 11:56:19 +0800

    feat(capacitor): update to latest directory layout

---
+++ packages/capacitor/ios/Plugin/Plugin.swift
@@ -0,0 +1,17 @@
+import Foundation
+import Capacitor
+
+/**
+ * Please read the Capacitor iOS Plugin Development Guide
+ * here: https://capacitor.ionicframework.com/docs/plugins/ios
+ */
+@objc(AdmobPlus)
+public class AdmobPlus: CAPPlugin {
+    
+    @objc func echo(_ call: CAPPluginCall) {
+        let value = call.getString("value") ?? ""
+        call.success([
+            "value": value
+        ])
+    }
+}
