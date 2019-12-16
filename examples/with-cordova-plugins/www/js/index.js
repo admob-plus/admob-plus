d54c37ecb089f03cccaeb02b63f8e08fa769c2fb
Ratson <github@ratson.name>
2019-12-16 22:25:41 +0800

    chore(example): create with-cordova-plugins example

---
+++ examples/with-cordova-plugins/www/js/index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const app = {
+  initialize() {
+    document.addEventListener(
+      'deviceready',
+      this.onDeviceReady.bind(this),
+      false,
+    )
+  },
+  onDeviceReady() {
+    this.receivedEvent('deviceready')
+  },
+  receivedEvent(id) {
+    const parentElement = document.getElementById(id)
+    const listeningElement = parentElement.querySelector('.listening')
+    const receivedElement = parentElement.querySelector('.received')
+
+    listeningElement.setAttribute('style', 'display:none;')
+    receivedElement.setAttribute('style', 'display:block;')
+
+    console.log(`Received Event: ${id}`)
+  },
+}
+
+app.initialize()
