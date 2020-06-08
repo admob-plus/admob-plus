15f26b765c80f5170b02fed9400579f6ee6c9770
David Koller <58932443+appit-online@users.noreply.github.com>
2020-06-08 12:57:00 +0200

    Update addBannerViewToView
    Update sendSubviewToBack to sendSubview method

--- packages/cordova/src/ios/AMSBanner.swift
+++ packages/cordova/src/ios/AMSBanner.swift
@@ -59,7 +59,7 @@ class AMSBanner: AMSAdBase, GADBannerViewDelegate {
             background.translatesAutoresizingMaskIntoConstraints = false
             background.backgroundColor = .black
             view.addSubview(background)
-            view.sendSubviewToBack(background)
+            view.sendSubview(toBack:background)
             NSLayoutConstraint.activate([
                 background.leadingAnchor.constraint(equalTo: view.leadingAnchor),
                 background.trailingAnchor.constraint(equalTo: view.trailingAnchor),
