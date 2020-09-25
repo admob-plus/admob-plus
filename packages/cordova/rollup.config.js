7863e48eef135dd08fe4ec655450e8c0e2517450
Ratson <github@ratson.name>
2020-09-26 00:20:03 +0800

    build(cordova): update rollup plugins

--- packages/cordova/rollup.config.js
+++ packages/cordova/rollup.config.js
@@ -1,13 +1,12 @@
-import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
-import typescript from 'rollup-plugin-typescript2'
-import findCacheDir from 'find-cache-dir'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
 
 export default {
   external: ['cordova'],
   input: './ts/admob.ts',
   output: {
-    file: 'www/admob.js',
+    dir: 'www',
     format: 'cjs',
     sourcemap: false,
     exports: 'default',
@@ -18,12 +17,8 @@ export default {
       browser: true,
     }),
     typescript({
-      cacheRoot: findCacheDir({ name: 'rts2' }),
-      tsconfigOverride: {
-        compilerOptions: {
-          module: 'es2015',
-        },
-      },
+      module: 'es2015',
+      composite: false,
     }),
     commonjs(),
   ],
