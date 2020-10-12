5040d453b694851d31fa7831d5ef133e7eb4b031
Ratson <github@ratson.name>
2020-10-12 20:49:21 +0800

    docs: update index page

--- website-v2/src/pages/index.js
+++ website-v2/src/pages/index.js
@@ -1,39 +1,78 @@
-import React from 'react'
-import clsx from 'clsx'
-import Layout from '@theme/Layout'
 import Link from '@docusaurus/Link'
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import useBaseUrl from '@docusaurus/useBaseUrl'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import Layout from '@theme/Layout'
+import clsx from 'clsx'
+import React from 'react'
 import styles from './styles.module.css'
 
 const features = [
   {
-    title: 'Easy to Use',
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    title: 'No Ad-Sharing',
+    imageUrl: 'img/saco.svg',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Unlike&nbsp;
+        <a href="https://github.com/appfeel/admob-google-cordova/blob/3f122f278a323a4bc9e580f400182a7bd690a346/src/android/AdMobAds.java#L569">
+          some
+        </a>
+        &nbsp;
+        <a href="https://github.com/sunnycupertino/cordova-plugin-admob-simple/blob/a58846c1ea14188a4aef44381ccd28ffdcae3bfa/src/android/AdMob.java#L207">
+          other
+        </a>
+        &nbsp;
+        <a href="https://github.com/floatinghotpot/cordova-admob-pro/wiki/License-Agreement#3-win-win-partnership">
+          plugins
+        </a>
+        , this plugin does not share your advertising revenue by randomly
+        display developer&apos;s owned ads.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    title: 'Fully Open Source',
+    imageUrl: 'img/open-source.svg',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Except Google provided&nbsp;
+        <a href="https://cocoapods.org/pods/Google-Mobile-Ads-SDK">
+          AdMob SDKs
+        </a>
+        , every line of code are on Github. You don&apos;t risk to&nbsp;
+        <a href="https://github.com/admob-google/admob-cordova/blob/master/src/android/libs/admobadplugin.jar">
+          execute
+        </a>
+        &nbsp;
+        <a href="https://unpkg.com/cordova-plugin-extension@1.5.4/src/android/cordova-generic-ad.jar">
+          compiled
+        </a>
+        &nbsp;
+        <a href="https://unpkg.com/cordova-plugin-extension@1.5.4/src/ios/libCordovaGenericAd.a">
+          binary
+        </a>
+        .
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    imageUrl: 'img/undraw_docusaurus_react.svg',
+    title: 'No Remote Control',
+    imageUrl: 'img/cute-locks.svg',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Do not&nbsp;
+        <a href="https://github.com/sunnycupertino/cordova-plugin-admob-simple/blob/f7cc64e9e018f2146b2735b5ae8d3b780fa24f72/src/android/AdMob.java#L728">
+          send
+        </a>
+        &nbsp;
+        <a href="https://forum.ionicframework.com/t/don-t-use-admob-pro-plugin-steal-30/90245">
+          your application information
+        </a>
+        &nbsp; to a remote server to control whether ad could be displayed.
+        Therefore, you don&apos;t lose revenue because{' '}
+        <a href="https://drive.google.com/file/d/0B5vtpya8P4b-NUZTdUhBVkFlU0E/view">
+          some server bugs
+        </a>
+        .
       </>
     ),
   },
@@ -84,9 +123,9 @@ function Home() {
           <section className={styles.features}>
             <div className="container">
               <div className="row">
-                {features.map((props, idx) => (
-                  // eslint-disable-next-line react/no-array-index-key,react/jsx-props-no-spreading
-                  <Feature key={idx} {...props} />
+                {features.map(props => (
+                  // eslint-disable-next-line react/jsx-props-no-spreading
+                  <Feature key={props.title} {...props} />
                 ))}
               </div>
             </div>
