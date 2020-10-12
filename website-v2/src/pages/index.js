7fbba5f86bd34a4cfc9e0dfebeec73500fbb127d
Ratson <github@ratson.name>
2020-10-12 20:54:07 +0800

    docs: add ExternalLink

--- website-v2/src/pages/index.js
+++ website-v2/src/pages/index.js
@@ -6,6 +6,12 @@ import clsx from 'clsx'
 import React from 'react'
 import styles from './styles.module.css'
 
+const ExternalLink = ({ href, children }) => (
+  <a href={href} rel="external nofollow">
+    {children}
+  </a>
+)
+
 const features = [
   {
     title: 'No Ad-Sharing',
@@ -13,17 +19,17 @@ const features = [
     description: (
       <>
         Unlike&nbsp;
-        <a href="https://github.com/appfeel/admob-google-cordova/blob/3f122f278a323a4bc9e580f400182a7bd690a346/src/android/AdMobAds.java#L569">
+        <ExternalLink href="https://github.com/appfeel/admob-google-cordova/blob/3f122f278a323a4bc9e580f400182a7bd690a346/src/android/AdMobAds.java#L569">
           some
-        </a>
+        </ExternalLink>
         &nbsp;
-        <a href="https://github.com/sunnycupertino/cordova-plugin-admob-simple/blob/a58846c1ea14188a4aef44381ccd28ffdcae3bfa/src/android/AdMob.java#L207">
+        <ExternalLink href="https://github.com/sunnycupertino/cordova-plugin-admob-simple/blob/a58846c1ea14188a4aef44381ccd28ffdcae3bfa/src/android/AdMob.java#L207">
           other
-        </a>
+        </ExternalLink>
         &nbsp;
-        <a href="https://github.com/floatinghotpot/cordova-admob-pro/wiki/License-Agreement#3-win-win-partnership">
+        <ExternalLink href="https://github.com/floatinghotpot/cordova-admob-pro/wiki/License-Agreement#3-win-win-partnership">
           plugins
-        </a>
+        </ExternalLink>
         , this plugin does not share your advertising revenue by randomly
         display developer&apos;s owned ads.
       </>
@@ -35,21 +41,21 @@ const features = [
     description: (
       <>
         Except Google provided&nbsp;
-        <a href="https://cocoapods.org/pods/Google-Mobile-Ads-SDK">
+        <ExternalLink href="https://cocoapods.org/pods/Google-Mobile-Ads-SDK">
           AdMob SDKs
-        </a>
+        </ExternalLink>
         , every line of code are on Github. You don&apos;t risk to&nbsp;
-        <a href="https://github.com/admob-google/admob-cordova/blob/master/src/android/libs/admobadplugin.jar">
+        <ExternalLink href="https://github.com/admob-google/admob-cordova/blob/master/src/android/libs/admobadplugin.jar">
           execute
-        </a>
+        </ExternalLink>
         &nbsp;
-        <a href="https://unpkg.com/cordova-plugin-extension@1.5.4/src/android/cordova-generic-ad.jar">
+        <ExternalLink href="https://unpkg.com/cordova-plugin-extension@1.5.4/src/android/cordova-generic-ad.jar">
           compiled
-        </a>
+        </ExternalLink>
         &nbsp;
-        <a href="https://unpkg.com/cordova-plugin-extension@1.5.4/src/ios/libCordovaGenericAd.a">
+        <ExternalLink href="https://unpkg.com/cordova-plugin-extension@1.5.4/src/ios/libCordovaGenericAd.a">
           binary
-        </a>
+        </ExternalLink>
         .
       </>
     ),
@@ -60,18 +66,18 @@ const features = [
     description: (
       <>
         Do not&nbsp;
-        <a href="https://github.com/sunnycupertino/cordova-plugin-admob-simple/blob/f7cc64e9e018f2146b2735b5ae8d3b780fa24f72/src/android/AdMob.java#L728">
+        <ExternalLink href="https://github.com/sunnycupertino/cordova-plugin-admob-simple/blob/f7cc64e9e018f2146b2735b5ae8d3b780fa24f72/src/android/AdMob.java#L728">
           send
-        </a>
+        </ExternalLink>
         &nbsp;
-        <a href="https://forum.ionicframework.com/t/don-t-use-admob-pro-plugin-steal-30/90245">
+        <ExternalLink href="https://forum.ionicframework.com/t/don-t-use-admob-pro-plugin-steal-30/90245">
           your application information
-        </a>
+        </ExternalLink>
         &nbsp; to a remote server to control whether ad could be displayed.
         Therefore, you don&apos;t lose revenue because{' '}
-        <a href="https://drive.google.com/file/d/0B5vtpya8P4b-NUZTdUhBVkFlU0E/view">
+        <ExternalLink href="https://drive.google.com/file/d/0B5vtpya8P4b-NUZTdUhBVkFlU0E/view">
           some server bugs
-        </a>
+        </ExternalLink>
         .
       </>
     ),
@@ -97,10 +103,7 @@ function Home() {
   const context = useDocusaurusContext()
   const { siteConfig = {} } = context
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
+    <Layout title={siteConfig.title} description={siteConfig.tagline}>
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
