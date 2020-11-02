8474945ff1fdc0290585230d3dcb828fb038a7f6
Ratson <github@ratson.name>
2020-11-03 01:29:01 +0800

    chore(playground): add banner control buttons

--- examples/playground/src/routes/Banner.tsx
+++ examples/playground/src/routes/Banner.tsx
@@ -2,21 +2,53 @@ import * as React from 'react'
 
 export interface BannerProps {}
 
+let bannerTop: any
+let banner: any
+
 const Banner: React.FC<BannerProps> = () => {
   React.useEffect(() => {
     // @ts-ignore
-    const bannerTop = new admob.BannerAd({
+    bannerTop = new admob.BannerAd({
       adUnitId: 'ca-app-pub-3940256099942544/6300978111',
     })
-    bannerTop.show({ position: 'top' })
     // @ts-ignore
-    const banner = new admob.BannerAd({
+    banner = new admob.BannerAd({
       adUnitId: 'ca-app-pub-3940256099942544/2934735716',
     })
-    banner.show({ position: 'bottom' })
   }, [])
 
-  return <p>BannerAd</p>
+  return (
+    <div>
+      <button
+        onClick={() => {
+          bannerTop.show({ position: 'top' })
+        }}
+      >
+        show top
+      </button>
+      <button
+        onClick={() => {
+          bannerTop.hide()
+        }}
+      >
+        hide top
+      </button>
+      <button
+        onClick={() => {
+          banner.show({ position: 'bottom' })
+        }}
+      >
+        show bottom
+      </button>
+      <button
+        onClick={() => {
+          banner.hide()
+        }}
+      >
+        hide bottom
+      </button>
+    </div>
+  )
 }
 
 export default Banner
