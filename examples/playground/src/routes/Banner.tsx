8170a33352c68abd74c365a0ae5e4e9141ddadd9
Ratson <github@ratson.name>
2020-11-06 15:00:39 +0800

    chore(playground): declare const admob as any

--- examples/playground/src/routes/Banner.tsx
+++ examples/playground/src/routes/Banner.tsx
@@ -7,11 +7,9 @@ let banner: any
 
 const Banner: React.FC<BannerProps> = () => {
   React.useEffect(() => {
-    // @ts-ignore
     bannerTop = new admob.BannerAd({
       adUnitId: 'ca-app-pub-3940256099942544/6300978111',
     })
-    // @ts-ignore
     banner = new admob.BannerAd({
       adUnitId: 'ca-app-pub-3940256099942544/2934735716',
     })
