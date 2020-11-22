96f6172bf9c4e0a546cca63c016241746ab2eeb0
Ratson <github@ratson.name>
2020-11-22 12:37:45 +0800

    test(cli): specify types in file

--- packages/cli/test/commands/info.test.ts
+++ packages/cli/test/commands/info.test.ts
@@ -1,10 +1,11 @@
+/// <reference types="@types/mocha" />
 import { expect, test } from '@oclif/test'
 
 describe('info', () => {
   test
     .stdout()
     .command(['info'])
-    .it('runs info', ctx => {
+    .it('runs info', (ctx) => {
       expect(ctx.stdout).to.contain('cordova')
     })
 })
