27b8ccc4cea00d0281ce91ef0109112a8d23d087
Ratson <github@ratson.name>
2021-01-16 02:30:47 +0800

    chore(cordova-android): remove deprecated ad request builder code

--- packages/cordova/src/android/Action.java
+++ packages/cordova/src/android/Action.java
@@ -86,23 +86,8 @@ public class Action {
     public AdRequest buildAdRequest() {
         Bundle extras = new Bundle();
         AdRequest.Builder builder = new AdRequest.Builder();
-        JSONArray testDevices = this.opts.optJSONArray("testDevices");
-        if (testDevices != null) {
-            for (int i = 0; i < testDevices.length(); i++) {
-                String testDevice = testDevices.optString(i);
-                if (testDevice != null) {
-                    builder.addTestDevice(testDevice);
-                }
-            }
-        }
-        if (this.opts.has("childDirected")) {
-            builder.tagForChildDirectedTreatment(opts.optBoolean("childDirected"));
-        }
         if (this.opts.has("npa")) {
             extras.putString("npa", opts.optString("npa"));
-        }
-        if (this.opts.has("underAgeOfConsent")) {
-            extras.putBoolean("tag_for_under_age_of_consent", opts.optBoolean("underAgeOfConsent"));
         }
         return builder.addNetworkExtrasBundle(AdMobAdapter.class, extras).build();
     }
