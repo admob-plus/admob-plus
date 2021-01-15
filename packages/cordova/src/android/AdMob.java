fb29dad074598b621a65179f9a40432457eeaf78
Ratson <github@ratson.name>
2021-01-16 02:34:53 +0800

    refactor(cordova-android): extract ok()

--- packages/cordova/src/android/AdMob.java
+++ packages/cordova/src/android/AdMob.java
@@ -19,18 +19,16 @@ import org.json.JSONObject;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 
+import admob.plugin.Generated.Actions;
 import admob.plugin.ads.AdBase;
 import admob.plugin.ads.BannerAd;
 import admob.plugin.ads.InterstitialAd;
 import admob.plugin.ads.RewardedVideoAd;
-import admob.plugin.Generated.Actions;
 
 public class AdMob extends CordovaPlugin {
     private static final String TAG = "AdMob-Plus";
-
-    private CallbackContext readyCallbackContext = null;
-
     private final ArrayList<PluginResult> eventQueue = new ArrayList<PluginResult>();
+    private CallbackContext readyCallbackContext = null;
 
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
@@ -53,9 +51,7 @@ public class AdMob extends CordovaPlugin {
             return executeReady(callbackContext);
         } else if (Actions.CONFIG_REQUEST.equals(actionKey)) {
             MobileAds.setRequestConfiguration(action.getRequestConfiguration());
-            PluginResult result = new PluginResult(PluginResult.Status.OK, "");
-            callbackContext.sendPluginResult(result);
-            return true;
+            return ok(callbackContext);
         } else if (Actions.BANNER_HIDE.equals(actionKey)) {
             return BannerAd.executeHideAction(action, callbackContext);
         } else if (Actions.BANNER_SHOW.equals(actionKey)) {
@@ -75,15 +71,11 @@ public class AdMob extends CordovaPlugin {
         } else if (Actions.SET_APP_MUTED.equals(actionKey)) {
             boolean value = args.optBoolean(0);
             MobileAds.setAppMuted(value);
-            PluginResult result = new PluginResult(PluginResult.Status.OK, "");
-            callbackContext.sendPluginResult(result);
-            return true;
+            return ok(callbackContext);
         } else if (Actions.SET_APP_VOLUME.equals(actionKey)) {
             float value = BigDecimal.valueOf(args.optDouble(0)).floatValue();
             MobileAds.setAppVolume(value);
-            PluginResult result = new PluginResult(PluginResult.Status.OK, "");
-            callbackContext.sendPluginResult(result);
-            return true;
+            return ok(callbackContext);
         }
 
         return false;
@@ -106,6 +98,12 @@ public class AdMob extends CordovaPlugin {
             e.printStackTrace();
         }
         emit(Generated.Events.READY, data);
+        return true;
+    }
+
+    private boolean ok(CallbackContext callbackContext) {
+        PluginResult result = new PluginResult(PluginResult.Status.OK, "");
+        callbackContext.sendPluginResult(result);
         return true;
     }
 
