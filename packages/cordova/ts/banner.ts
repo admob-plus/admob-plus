6984a1819b9ccc70b034069f45d3ab85c763e7de
Ratson <github@ratson.name>
2021-01-17 02:00:39 +0800

    chore(cordova-android): change banner position to use union type

--- packages/cordova/ts/banner.ts
+++ packages/cordova/ts/banner.ts
@@ -1,10 +1,7 @@
 import type { MobileAdOptions } from './base'
 import { AdSizeType, execAsync, MobileAd, NativeActions } from './base'
 
-enum Position {
-  top = 'top',
-  bottom = 'bottom',
-}
+type Position = 'top' | 'bottom'
 
 type Options = {
   position?: Position
@@ -12,20 +9,19 @@ type Options = {
 }
 
 export default class BannerAd extends MobileAd {
-  public readonly Position = Position
   private opts: Options
 
   constructor({ adUnitId, ...opts }: MobileAdOptions & Options) {
     super({ adUnitId })
 
     this.opts = {
-      position: Position.bottom,
+      position: 'bottom',
       size: AdSizeType.SMART_BANNER,
       ...opts,
     }
   }
 
-  public show(opts: Options) {
+  public show() {
     return execAsync(NativeActions.bannerShow, [
       {
         ...this.opts,
