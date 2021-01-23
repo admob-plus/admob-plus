6dd50914b731b1b517701a8e90a2db38733ec784
Ratson <github@ratson.name>
2021-01-23 11:11:50 +0800

    feat(cordova-consent): generate constants

--- internal/cli/src/gen/consent.ts
+++ internal/cli/src/gen/consent.ts
@@ -1,15 +1,84 @@
+import _ from 'lodash'
 import { pkgsDirJoin } from '../utils'
-import { buildUtils, warnMessage } from './shared'
+import {
+  buildUtils,
+  renderJavaContants,
+  renderSwiftContants,
+  renderTsContants,
+  warnMessage,
+} from './shared'
+
+const Actions = _.mapValues(
+  {
+    ready: null,
+    requestInfoUpdate: null,
+    isFormAvailable: null,
+    loadForm: null,
+    getStatus: null,
+    showForm: null,
+    reset: null,
+  },
+  (v, k) => (v === null ? k : v) as string,
+)
+
+const Events = _.mapValues(
+  { ready: null },
+  (v, k) => `consent.${v === null ? k : v}`,
+)
+
+function buildJava(): string {
+  const linesActions = renderJavaContants(Actions)
+  const linesEvents = renderJavaContants(Events)
+
+  return `// ${warnMessage}
+package cordova.plugin.consent;
+
+public final class Generated {
+    public final class Actions {
+${linesActions}
+    }
+
+    public final class Events {
+${linesEvents}
+    }
+}
+`
+}
+
+function buildSwift(): string {
+  const linesEvents = renderSwiftContants(Events)
+
+  return `// ${warnMessage}
+struct AMCEvents {
+${linesEvents}
+}
+`
+}
 
 function buildTypeScript(): string {
   return `// ${warnMessage}
 import { exec } from 'cordova'
+
+export enum NativeActions {
+${renderTsContants(Actions)}
+}
+
+export enum Events {
+${renderTsContants(Events)}
+}
 ${buildUtils('Consent')}
 `
 }
 
 export default () => ({
-  files: [{ path: 'cordova-consent/ts/generated.ts', f: buildTypeScript }],
+  files: [
+    { path: 'cordova-consent/src/android/Generated.java', f: buildJava },
+    {
+      path: 'cordova-consent/src/ios/AMCGenerated.swift',
+      f: buildSwift,
+    },
+    { path: 'cordova-consent/ts/generated.ts', f: buildTypeScript },
+  ],
   pkgDir: pkgsDirJoin('cordova-consent'),
   tagertDir: 'src/cordova/plugin/consent',
 })
