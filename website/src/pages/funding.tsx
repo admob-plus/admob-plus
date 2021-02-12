ce27440836f73093ed27a923825d83ed9b042034
Ratson <github@ratson.name>
2021-02-12 20:13:42 +0800

    docs: update funding page title

--- website/src/pages/funding.tsx
+++ website/src/pages/funding.tsx
@@ -7,24 +7,23 @@ const Funding: React.FC = () => {
   const context = useDocusaurusContext()
   const { siteConfig = {} } = context
   return (
-    <Layout title={`Funding - ${siteConfig.title}`}>
+    <Layout title="Funding">
       <main className="container margin-vert--lg">
         <div className="text--center margin-bottom--lg">
-          <h1>Funding AdMob-Plus!</h1>
+          <h1>Funding AdMob Plus!</h1>
         </div>
         <div className="row margin-bottom--lg">
           <div className="post">
             <header className="postHeader">
-              <h2>Supporting AdMob-Plus</h2>
+              <h2>Supporting AdMob Plus</h2>
             </header>
             <p>
-              AdMob-Plus is an MIT-licensed open source project. It&apos;s an
+              AdMob Plus is an MIT-licensed open source project. It&apos;s an
               independent project maintained by{' '}
               <a href="https://github.com/ratson">@ratson</a>.
             </p>
             <p>Your support will fund him to</p>
             <ul>
-              <li>Maintain cordova-plugin-admob-free and its support.</li>
               <li>
                 Develop AdMob Plus with faster releases, more features, and
                 higher quality.
