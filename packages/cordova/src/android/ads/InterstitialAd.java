e73940f1d23c3d1fdf4b7be7264dbd0be42c645f
Ratson <github@ratson.name>
2021-03-01 01:57:28 +0800

    feat(cordova/android): use com.google.android.gms.ads.interstitial.InterstitialAd

--- packages/cordova/src/android/ads/InterstitialAd.java
+++ packages/cordova/src/android/ads/InterstitialAd.java
@@ -1,14 +1,18 @@
 package admob.plugin.ads;
 
-import com.google.android.gms.ads.AdListener;
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.ads.AdError;
 import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.FullScreenContentCallback;
 import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 
 import admob.plugin.Action;
 import admob.plugin.Generated.Events;
 
 public class InterstitialAd extends AdBase {
-    private com.google.android.gms.ads.InterstitialAd interstitialAd = null;
+    private com.google.android.gms.ads.interstitial.InterstitialAd mInterstitialAd = null;
 
     InterstitialAd(int id, String adUnitId) {
         super(id, adUnitId);
@@ -32,56 +36,53 @@ public class InterstitialAd extends AdBase {
     public void load(AdRequest adRequest, String adUnitId) {
         clear();
 
-        interstitialAd = new com.google.android.gms.ads.InterstitialAd(getActivity());
-        interstitialAd.setAdUnitId(adUnitId);
-        interstitialAd.setAdListener(new AdListener() {
+        com.google.android.gms.ads.interstitial.InterstitialAd.load(getActivity(), adUnitId, adRequest, new InterstitialAdLoadCallback() {
             @Override
-            public void onAdClicked() {
-                plugin.emit(Events.INTERSTITIAL_CLICK);
-            }
+            public void onAdLoaded(@NonNull com.google.android.gms.ads.interstitial.InterstitialAd interstitialAd) {
+                mInterstitialAd = interstitialAd;
+                mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+                    @Override
+                    public void onAdDismissedFullScreenContent() {
+                        plugin.emit(Events.INTERSTITIAL_CLOSE);
+                    }
 
-            @Override
-            public void onAdClosed() {
-                plugin.emit(Events.INTERSTITIAL_CLOSE);
-            }
+                    @Override
+                    public void onAdFailedToShowFullScreenContent(AdError adError) {
+                        // TODO
+                    }
 
-            @Override
-            public void onAdFailedToLoad(LoadAdError error) {
-                plugin.emit(Events.INTERSTITIAL_LOAD_FAIL, buildErrorPayload(error.getCode()));
-            }
+                    @Override
+                    public void onAdShowedFullScreenContent() {
+                        plugin.emit(Events.INTERSTITIAL_OPEN);
+                        mInterstitialAd = null;
+                    }
+                });
 
-            @Override
-            public void onAdImpression() {
-                plugin.emit(Events.INTERSTITIAL_IMPRESSION);
-            }
-
-            @Override
-            public void onAdLoaded() {
                 plugin.emit(Events.INTERSTITIAL_LOAD);
             }
 
             @Override
-            public void onAdOpened() {
-                plugin.emit(Events.INTERSTITIAL_OPEN);
+            public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                mInterstitialAd = null;
+                plugin.emit(Events.INTERSTITIAL_LOAD_FAIL, buildErrorPayload(loadAdError.getCode()));
             }
         });
-        interstitialAd.loadAd(adRequest);
     }
 
     public boolean isLoaded() {
-        return interstitialAd != null && interstitialAd.isLoaded();
+        return mInterstitialAd != null
     }
 
     public void show() {
         if (isLoaded()) {
-            interstitialAd.show();
+            mInterstitialAd.show(getActivity());
         }
     }
 
     private void clear() {
-        if (interstitialAd != null) {
-            interstitialAd.setAdListener(null);
-            interstitialAd = null;
+        if (mInterstitialAd != null) {
+            mInterstitialAd.setFullScreenContentCallback(null);
+            mInterstitialAd = null;
         }
     }
 }
