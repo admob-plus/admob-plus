ef7583375ff8ee48b28608ba4fa48b29e39243c5
Ratson <github@ratson.name>
2021-03-07 08:50:18 +0800

    feat(cli): align indentation of printed text

--- packages/cli/src/cmds/info.ts
+++ packages/cli/src/cmds/info.ts
@@ -1,7 +1,6 @@
 import * as clipboardy from 'clipboardy'
 import * as elementtree from 'elementtree'
-// @ts-expect-error: no types
-import * as envinfo from 'envinfo'
+import * as envinfo from '@frat/envinfo'
 import * as execa from 'execa'
 import * as fse from 'fs-extra'
 import * as _ from 'lodash'
@@ -17,21 +16,28 @@ const collectionAndroidManifestInfo = async () => {
   const items = etree.findall(
     './application/meta-data/[@android:name="com.google.android.gms.ads.APPLICATION_ID"]',
   )
-  return {
-    count: items.length,
-    toString() {
-      switch (items.length) {
-        case 0:
-          return 'missing <meta-data android:name="com.google.android.gms.ads.APPLICATION_ID" ... />'
-        case 1:
-          return items[0].get('android:value') === 'test'
-            ? 'using test APPLICATION_ID'
-            : 'looks ok'
-        default:
-          return 'multiple <meta-data android:name="com.google.android.gms.ads.APPLICATION_ID" ... />'
-      }
-    },
+
+  const result: { [k: string]: string } = {}
+  const appIdKey =
+    '<meta-data android:name="com.google.android.gms.ads.APPLICATION_ID" ... />'
+
+  result[appIdKey] = 'missing'
+  switch (items.length) {
+    case 0:
+      result[appIdKey] = 'missing'
+      break
+    case 1: {
+      const appId = items[0].get('android:value') || '-'
+      result[appIdKey] = ['ca-app-pub-xxx~yyy', 'test', '-'].includes(appId)
+        ? appId
+        : 'looks ok'
+      break
+    }
+    default:
+      result[appIdKey] = 'multiple'
   }
+
+  return result
 }
 
 export const command = 'info'
@@ -49,6 +55,37 @@ export const builder = (yargs: Argv<unknown>) =>
 
 export const handler = async (argv: ReturnType<typeof builder>['argv']) => {
   const pkg = { ...(await readPkgUp())?.packageJson }
+  const extraInfo: { [x: string]: unknown } = {}
+
+  const { stdout: cordovaVersion } = await execa('cordova', ['--version'], {
+    reject: false,
+  })
+
+  const deps: { [k: string]: string } = {
+    ...pkg.devDependencies,
+    ...pkg.dependencies,
+  }
+  const cordovaPlugins = _.reduce(
+    _.get(pkg, 'cordova.plugins'),
+    (acc, v, k) => ({ ...acc, [k]: deps[k] }),
+    {},
+  )
+  extraInfo.Cordova = {
+    Version: cordovaVersion || '-',
+    Plugins: cordovaPlugins,
+  }
+  const { stdout: ionicVersion } = await execa('ionic', ['--version'], {
+    reject: false,
+  })
+  extraInfo.Ionic = { Version: ionicVersion || '-' }
+
+  try {
+    const androidInfo = await collectionAndroidManifestInfo()
+    extraInfo['AndroidManifest.xml'] = androidInfo
+  } catch {
+    extraInfo['AndroidManifest.xml'] = { Version: '-' }
+  }
+
   let infoText = ''
   try {
     infoText = await envinfo.run(
@@ -78,44 +115,14 @@ export const handler = async (argv: ReturnType<typeof builder>['argv']) => {
       {
         console: false,
         title: 'AdMob Plus Environment Info',
+        transform(x) {
+          return { ...x, ...extraInfo }
+        },
       },
     )
   } catch (err) {
     console.error(err)
   }
-
-  if (!infoText.includes('cordova')) {
-    const { stdout: cordovaVersion } = await execa('cordova', ['--version'], {
-      reject: false,
-    })
-    infoText += `\ncordova: ${cordovaVersion}\n`
-
-    const { stdout: ionicVersion } = await execa('ionic', ['--version'], {
-      reject: false,
-    })
-    infoText += `ionic: ${ionicVersion}\n`
-
-    const deps: { [k: string]: string } = {
-      ...pkg.devDependencies,
-      ...pkg.dependencies,
-    }
-    const cordovaPlugins = _.reduce(
-      _.get(pkg, 'cordova.plugins'),
-      (acc, v, k) => ({ ...acc, [k]: deps[k] }),
-      {},
-    )
-    infoText += `cordova plugins: ${JSON.stringify(
-      cordovaPlugins,
-      undefined,
-      2,
-    )}\n`
-  }
-
-  try {
-    const androidInfo = await collectionAndroidManifestInfo()
-    infoText += `\nAndroidManifest.xml: ${androidInfo}\n`
-    // tslint:disable-next-line:no-empty
-  } catch {}
 
   console.log(infoText)
 
