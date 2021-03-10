3b575ec7729ac760fd8aa80e0a4064b14ca22386
Ratson <github@ratson.name>
2021-03-10 19:50:01 +0800

    chore(example/playground): remove isTrusted from log line

--- examples/playground/src/components/Logs.tsx
+++ examples/playground/src/components/Logs.tsx
@@ -1,17 +1,25 @@
-import * as React from 'react'
 import { Events } from 'admob-plus-cordova'
+import * as React from 'react'
 
-const eventTypes = [
-  'deviceready',
-  ...Object.keys(Events).map((k) => (Events as { [x: string]: string })[k]),
-]
+const eventTypes = ['deviceready', ...Object.values(Events)]
+
+type Log = { time: Date; event: Event }
+
+const logLine = (log: Log) => {
+  const s = JSON.stringify({
+    ...log.event,
+    isTrusted: undefined,
+  })
+  if (s === '{}') {
+    return log.event.type
+  }
+  return `${log.event.type}: ${s}`
+}
 
 export interface LogsProps {}
 
 const Logs: React.FC<LogsProps> = () => {
-  const [logs, setLogs] = React.useState<Array<{ time: Date; event: Event }>>(
-    [],
-  )
+  const [logs, setLogs] = React.useState<Array<Log>>([])
 
   React.useEffect(() => {
     const listener = (event: Event) => {
@@ -38,9 +46,7 @@ const Logs: React.FC<LogsProps> = () => {
   return (
     <div>
       {logs.map((log, i) => (
-        <div key={i}>
-          {log.event.type}: {JSON.stringify(log.event)}
-        </div>
+        <div key={i}>{logLine(log)}</div>
       ))}
     </div>
   )
