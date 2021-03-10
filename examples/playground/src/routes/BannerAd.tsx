1df062ba2c372ad9e19e6ee9120f1d89a6d58435
Ratson <github@ratson.name>
2021-03-10 23:52:40 +0800

    fix(example/playground): create banners once only

--- examples/playground/src/routes/BannerAd.tsx
+++ examples/playground/src/routes/BannerAd.tsx
@@ -1,21 +1,21 @@
-import { Button, ButtonGroup, Box } from '@chakra-ui/react'
+import { Box, Button, ButtonGroup } from '@chakra-ui/react'
+import _ from 'lodash'
 import * as React from 'react'
+
+const createBanners = _.memoize(() => ({
+  top: new admob.BannerAd({
+    adUnitId: 'ca-app-pub-3940256099942544/6300978111',
+    position: 'top',
+  }),
+  bottom: new admob.BannerAd({
+    adUnitId: 'ca-app-pub-3940256099942544/6300978111',
+  }),
+}))
 
 export interface BannerProps {}
 
 const BannerAd: React.FC<BannerProps> = () => {
-  const banners = React.useMemo(
-    () => ({
-      top: new admob.BannerAd({
-        adUnitId: 'ca-app-pub-3940256099942544/6300978111',
-        position: 'top',
-      }),
-      bottom: new admob.BannerAd({
-        adUnitId: 'ca-app-pub-3940256099942544/6300978111',
-      }),
-    }),
-    [],
-  )
+  const banners = createBanners()
 
   return (
     <div>
