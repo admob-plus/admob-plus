7ff212a3fbc64fe26984122d941b49d3475ffa8c
Ratson <github@ratson.name>
2021-03-14 14:26:35 +0800

    feat(example/ionic-angular): show ads on button clicks

--- examples/ionic-angular/src/app/home/home.page.ts
+++ examples/ionic-angular/src/app/home/home.page.ts
@@ -11,23 +11,29 @@ export class HomePage {
   constructor(private platform: Platform, private admob: AdMob) {
     this.platform.ready().then(async () => {
       await admob.start();
-
-      const banner = new admob.BannerAd({
-        adUnitId: 'ca-app-pub-3940256099942544/6300978111',
-      });
-      await banner.show();
-
-      const interstitial = new admob.InterstitialAd({
-        adUnitId: 'ca-app-pub-3940256099942544/1033173712',
-      });
-      await interstitial.load();
-      await interstitial.show();
-
-      const rewarded = new admob.RewardedAd({
-        adUnitId: 'ca-app-pub-3940256099942544/5224354917',
-      });
-      await rewarded.load();
-      await rewarded.show();
     });
+  }
+
+  async showBannerAd() {
+    const banner = new this.admob.BannerAd({
+      adUnitId: 'ca-app-pub-3940256099942544/6300978111',
+    });
+    await banner.show();
+  }
+
+  async showInterstitialAd() {
+    const interstitial = new this.admob.InterstitialAd({
+      adUnitId: 'ca-app-pub-3940256099942544/1033173712',
+    });
+    await interstitial.load();
+    await interstitial.show();
+  }
+
+  async showRewardedAd() {
+    const rewarded = new this.admob.RewardedAd({
+      adUnitId: 'ca-app-pub-3940256099942544/5224354917',
+    });
+    await rewarded.load();
+    await rewarded.show();
   }
 }
