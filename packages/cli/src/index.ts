e16526150a2868fb06973c58aaea281c702fa908
Ratson <github@ratson.name>
2021-03-26 22:52:12 +0800

    chore(cli): set script name

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -3,8 +3,9 @@ import { hideBin } from 'yargs/helpers'
 import * as doctorCmd from './cmds/doctor'
 import * as infoCmd from './cmds/info'
 
-export default () => {
+export default (name: string) => {
   const { argv } = yargs(hideBin(process.argv))
+    .scriptName(name)
     .command(doctorCmd)
     .command(infoCmd)
     .demandCommand()
