c4bc0d89741d24c364d7a0b2ca14780f73e19464
Ratson <github@ratson.name>
2021-03-27 12:39:37 +0800

    chore(ionic): relax peerDependencies requirements

--- packages/ionic/package.test.ts
+++ packages/ionic/package.test.ts
@@ -1,4 +1,7 @@
 test('peerDependencies', () => {
   const pkg = require('./package.json')
   expect(pkg.peerDependencies['admob-plus-cordova']).toBe('^1.0.0')
+
+  const pkgRef = require('@ionic-native/admob-plus//package.json')
+  expect(pkg.peerDependencies).toMatchObject(pkgRef.peerDependencies)
 })
