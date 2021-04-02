2ed774b6ef23da1b9a00eb202f6ce5559eceb85b
Ratson <github@ratson.name>
2021-04-02 23:35:12 +0800

    feat(cli/doctor): check ad server connection

--- packages/cli/src/doctor/index.ts
+++ packages/cli/src/doctor/index.ts
@@ -1,17 +1,35 @@
-import { Listr } from 'listr2'
+import got from 'got'
+import { Listr, ListrTask } from 'listr2'
 import taskCocoapods from './cocoapods'
 import tasksCordova from './cordova'
 import { Ctx, options as listrOptions } from './listr'
 import tasksNode, { readPackageJson } from './node'
 
+const taskConnection: ListrTask = {
+  async task(_ctx, task) {
+    const host = 'googleads.g.doubleclick.net'
+    task.title = host
+    try {
+      await got(`https://${host}`)
+    } catch (err) {
+      task.output = `${err}`
+      throw new Error(host)
+    }
+  },
+  options: { persistentOutput: true },
+}
+
 export default async () => {
   const pkg = await readPackageJson('package.json')
   const ctx: Ctx = { pkg }
 
-  const tasks = new Listr<Ctx>([taskCocoapods, ...tasksNode, ...tasksCordova], {
-    ...listrOptions,
-    ctx,
-  })
+  const tasks = new Listr<Ctx>(
+    [taskConnection, taskCocoapods, ...tasksNode, ...tasksCordova],
+    {
+      ...listrOptions,
+      ctx,
+    },
+  )
 
   try {
     await tasks.run()
