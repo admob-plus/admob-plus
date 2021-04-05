e4085282fe79ee3c70fa8191fe15e5f7a5564177
Ratson <github@ratson.name>
2021-04-06 00:33:45 +0800

    feat(react-native/ios): implement start()

--- packages/react-native/ios/RNAdMobPlus.swift
+++ packages/react-native/ios/RNAdMobPlus.swift
@@ -1,7 +1,10 @@
+import GoogleMobileAds
+
 @objc(RNAdMobPlus)
 class RNAdMobPlus: NSObject {
-    @objc func start(_ resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) {
-        print("started")
-        resolve("started")
+    @objc func start(_ resolve: @escaping (RCTPromiseResolveBlock), rejecter reject: RCTPromiseRejectBlock) {
+        GADMobileAds.sharedInstance().start(completionHandler: { _ in
+            resolve(["version": GADMobileAds.sharedInstance().sdkVersion])
+        })
     }
 }
