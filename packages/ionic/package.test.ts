912ce685cb0142b5e788417bdee24ce572555ea2
Ratson <github@ratson.name>
2021-04-05 23:52:27 +0800

    test: check pkg-deps before commit

--- packages/ionic/package.test.ts
+++
@@ -1,7 +0,0 @@
-test('peerDependencies', () => {
-  const pkg = require('./package.json')
-  expect(pkg.peerDependencies['admob-plus-cordova']).toBe('^1.0.0')
-
-  const pkgRef = require('@ionic-native/admob-plus//package.json')
-  expect(pkg.peerDependencies).toMatchObject(pkgRef.peerDependencies)
-})
