6a4e75c9c4fde5aa0f3ede539454cce3763da5d8
Ratson <github@ratson.name>
2021-05-02 23:31:58 +0800

    build(ionic): remove tsconfig.build.json

--- packages/ionic/scripts/build.ts
+++ packages/ionic/scripts/build.ts
@@ -5,23 +5,23 @@ import * as fse from 'fs-extra'
 const main = async () => {
   await del(['build', 'lib', 'ngx', '*.tsbuildinfo'])
 
+  const s = await fse.readFile('./src/ngx/index.ts', 'utf8')
+  await fse.outputFile(
+    './src/lib/index.ts',
+    s
+      .replace("import { Injectable } from '@angular/core'\n", '')
+      .replace('@Injectable()\n', ''),
+  )
+
   await execa('tsc', { stdio: 'inherit' })
+  await fse.copy('build/lib', 'lib', { overwrite: true })
+  await fse.copy('build/ngx', 'ngx', { overwrite: true })
+
   await fse.outputJSON('./ngx/package.json', {
     ...require('../package.json'),
     typings: 'index.d.ts',
     main: 'index.js',
     module: 'index.js',
-  })
-
-  const s = await fse.readFile('./src/index.ts', 'utf8')
-  await fse.outputFile(
-    './build/index.ts',
-    s
-      .replace("import { Injectable } from '@angular/core'\n", '')
-      .replace('@Injectable()\n', ''),
-  )
-  await execa('tsc', ['-p', 'tsconfig.build.json'], {
-    stdio: 'inherit',
   })
 }
 
