9a92f4333553dd60a4cbd7b2d7adc27b32add13e
Ratson <github@ratson.name>
2021-06-07 21:17:19 +0800

    fix(cordova/android): expose Helper.ads

--- packages/react-native/android/src/main/java/admob/plus/core/Helper.java
+++ packages/react-native/android/src/main/java/admob/plus/core/Helper.java
@@ -24,7 +24,7 @@ import java.util.List;
 import java.util.Map;
 
 public class Helper {
-    static final SparseArray<Ad> ads = new SparseArray<Ad>();
+    public static final SparseArray<Ad> ads = new SparseArray<Ad>();
     private final Adapter adapter;
 
     public Helper(Adapter adapter) {
