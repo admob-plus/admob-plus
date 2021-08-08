c21331a4d659c8754a7b8297ad087dd7443320ad
Ratson <github@ratson.name>
2021-08-08 09:49:56 +0800

    feat(example/ionic-angular-capacitor): add ad buttons

--- examples/ionic-angular-capacitor/src/app/app.module.ts
+++ examples/ionic-angular-capacitor/src/app/app.module.ts
@@ -11,7 +11,9 @@ import { AppComponent } from './app.component';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- examples/ionic-angular-capacitor/src/app/tab1/tab1.page.ts
+++ examples/ionic-angular-capacitor/src/app/tab1/tab1.page.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import {
+  BannerAd,
+  InterstitialAd,
+  RewardedAd,
+  RewardedInterstitialAd,
+} from '@admob-plus/capacitor';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
   constructor() {}
 
+  async showBannerAd() {
+    const banner = new BannerAd({
+      adUnitId: 'ca-app-pub-3940256099942544/6300978111',
+      position: 'top',
+    });
+    await banner.show();
+  }
+
+  async showInterstitialAd() {
+    const interstitial = new InterstitialAd({
+      adUnitId: 'ca-app-pub-3940256099942544/1033173712',
+    });
+    await interstitial.load();
+    await interstitial.show();
+  }
+
+  async showRewardedAd() {
+    const rewarded = new RewardedAd({
+      adUnitId: 'ca-app-pub-3940256099942544/5224354917',
+    });
+    await rewarded.load();
+    await rewarded.show();
+  }
+
+  async showRewardedInterstitialAd() {
+    const rewarded = new RewardedInterstitialAd({
+      adUnitId: 'ca-app-pub-3940256099942544/6978759866',
+    });
+    await rewarded.load();
+    await rewarded.show();
+  }
 }
