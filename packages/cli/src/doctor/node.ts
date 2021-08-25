678fe3fd9551aeba76bdd398120e21ca0468b202
Ratson <github@ratson.name>
2021-08-25 14:21:45 +0800

    feat(cli): check Ionic Capacitor project using Cordova packages

--- packages/cli/src/doctor/node.ts
+++ packages/cli/src/doctor/node.ts
@@ -14,27 +14,40 @@ export default [
       }
 
       const appIdKeys = ['APP_ID_ANDROID', 'APP_ID_IOS']
-      return task.newListr(
-        _.flatMap(appIdKeys, (k) => {
-          const configPath = `cordova.plugins.admob-plus-cordova.${k}`
-          const appId = _.get(pkg, configPath)
-          if (!appId) {
-            return []
-          }
+      const tasks: ListrTask<Ctx, any>[] = _.flatMap(appIdKeys, (k) => {
+        const configPath = `cordova.plugins.admob-plus-cordova.${k}`
+        const appId = _.get(pkg, configPath)
+        if (!appId) {
+          return []
+        }
 
-          return {
-            title: configPath,
-            async task(_ctx, taskConfig) {
-              if (testAppIds.has(appId)) {
-                taskConfig.output = `${appId} is a test ID`
-                throw new Error(configPath)
-              }
-            },
-            options: { persistentOutput: true },
-          }
-        }),
-        { concurrent: true },
-      )
+        return {
+          title: configPath,
+          async task(_ctx, taskConfig) {
+            if (testAppIds.has(appId)) {
+              taskConfig.output = `${appId} is a test ID`
+              throw new Error(configPath)
+            }
+          },
+          options: { persistentOutput: true },
+        }
+      })
+      if (pkg.depends('@ionic/angular')) {
+        tasks.push({
+          title: '@ionic/angular',
+          async task(_ctx, taskIonic) {
+            if (
+              pkg.depends('@capacitor/core') &&
+              pkg.dependsAny('admob-plus-cordova', '@admob-plus/ionic')
+            ) {
+              taskIonic.output = 'Install @admob-plus/capacitor instead'
+              throw new Error('Using Cordova packages in Capacitor project')
+            }
+          },
+          options: { persistentOutput: true },
+        })
+      }
+      return task.newListr(tasks, { concurrent: true })
     },
   },
 ] as ListrTask<Ctx>[]
