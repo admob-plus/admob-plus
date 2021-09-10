dee1c3ae5256a51d7396d243fe5e2bb2685aa48e
Ratson <github@ratson.name>
2021-09-11 00:52:52 +0800

    feat(example/ionic3): add @admob-plus/ionic

--- examples/ionic3/src/app/app.module.ts
+++ examples/ionic3/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { AdMob } from '@admob-plus/ionic/ngx';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
@@ -32,6 +33,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TabsPage
   ],
   providers: [
+    AdMob,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- examples/ionic3/src/pages/home/home.ts
+++ examples/ionic3/src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AdMob, BannerAd } from '@admob-plus/ionic/ngx';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  banner: BannerAd
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private admob: AdMob) {
   }
 
+  async showBannerAd() {
+    const banner = new this.admob.BannerAd({
+      adUnitId: 'ca-app-pub-3940256099942544/6300978111',
+    });
+    this.banner = banner
+    await banner.show();
+  }
+
+  async hideBannerAd() {
+    await this.banner?.hide();
+  }
 }
