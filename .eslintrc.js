3fcfaef5267a49ff5c970f0616c84076e1675464
Ratson <github@ratson.name>
2022-04-18 02:02:09 +0800

    build(eslint): disable node/no-unsupported-features/es-syntax rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,26 +6,15 @@ module.exports = {
       files: ['__tests__/**/*.ts'],
       rules: {
         '@typescript-eslint/no-var-requires': 'warn',
-        'node/no-missing-require': 'warn',
-      },
-    },
-    {
-      files: ['packages/cordova/src/browser/AdMobProxy.js'],
-      rules: {
-        strict: 'off',
       },
     },
   ],
   rules: {
-    'function-paren-newline': 'off',
     'max-classes-per-file': 'off',
     'node/no-extraneous-import': 'warn',
     'node/no-extraneous-require': 'warn',
     'node/no-unpublished-import': 'warn',
     'node/no-unpublished-require': 'warn',
-    'node/no-unsupported-features/es-syntax': [
-      'warn',
-      {version: '>=14', ignores: []},
-    ],
+    'node/no-unsupported-features/es-syntax': 'off',
   },
 };
