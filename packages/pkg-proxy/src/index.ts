1491afe5f96c1d2f4f6ace6b431b71cd35243d50
Ratson <github@ratson.name>
2022-05-07 12:31:00 +0800

    chore(pkg-proxy): format code

--- packages/pkg-proxy/src/index.ts
+++ packages/pkg-proxy/src/index.ts
@@ -1,91 +1,91 @@
-import escalade from 'escalade'
-import fse from 'fs-extra'
-import path from 'path'
-import { PackageJson as PackageJsonBase } from 'types-package-json'
+import escalade from 'escalade';
+import fse from 'fs-extra';
+import path from 'path';
+import {PackageJson as PackageJsonBase} from 'types-package-json';
 
-export type PackageJsonFileName = 'package.json'
+export type PackageJsonFileName = 'package.json';
 
 export interface PackageJson extends PackageJsonBase {
-  [k: string]: any
+  [k: string]: any;
 }
 
-export const fileName: PackageJsonFileName = 'package.json'
+export const fileName: PackageJsonFileName = 'package.json';
 
 type Options = {
-  cwd?: string
-  filename?: string
-  searchParents?: boolean
-}
+  cwd?: string;
+  filename?: string;
+  searchParents?: boolean;
+};
 
 async function findPackageJson(opts?: Options) {
-  const cwd = opts?.cwd ?? process.cwd()
-  const filename = opts?.filename ?? path.join(cwd, fileName)
+  const cwd = opts?.cwd ?? process.cwd();
+  const filename = opts?.filename ?? path.join(cwd, fileName);
   if (await fse.pathExists(filename)) {
-    return filename
+    return filename;
   }
   if (opts?.searchParents) {
     const result = await escalade(
       cwd,
-      (_dir, names) => names.includes(fileName) && fileName,
-    )
-    if (result) return result
+      (_dir, names) => names.includes(fileName) && fileName
+    );
+    if (result) return result;
   }
 }
 
-export type PkgProxy = PackageJson & Pkg
+export type PkgProxy = PackageJson & Pkg;
 
 export class Pkg {
   static async createProxy(opts?: Options) {
-    const pkg = await this.find(opts)
-    if (!pkg) return undefined
+    const pkg = await this.find(opts);
+    if (!pkg) return undefined;
 
     return new Proxy(pkg.json, {
       get(target, prop, receiver) {
         if (prop in pkg) {
-          return Reflect.get(pkg, prop, receiver)
+          return Reflect.get(pkg, prop, receiver);
         }
-        return Reflect.get(target, prop, receiver)
+        return Reflect.get(target, prop, receiver);
       },
-    }) as PkgProxy
+    }) as PkgProxy;
   }
 
   static async find(opts?: Options) {
-    const filename = await findPackageJson(opts)
-    if (filename) return this.read(filename)
+    const filename = await findPackageJson(opts);
+    if (filename) return this.read(filename);
   }
 
   static async read(filename: string) {
-    const pkg: PackageJson = await fse.readJson(filename)
-    return new Pkg(filename, pkg)
+    const pkg: PackageJson = await fse.readJson(filename);
+    return new Pkg(filename, pkg);
   }
 
-  readonly filename: string
-  readonly json: PackageJson
+  readonly filename: string;
+  readonly json: PackageJson;
 
   constructor(filename: string, pkg: PackageJson) {
-    this.filename = filename
-    this.json = pkg
+    this.filename = filename;
+    this.json = pkg;
   }
 
   get dependencies() {
-    return this.json.dependencies ?? {}
+    return this.json.dependencies ?? {};
   }
 
   get devDependencies() {
-    return this.json.devDependencies ?? {}
+    return this.json.devDependencies ?? {};
   }
 
   depends(name: string) {
-    return Boolean(this.dependencies[name] || this.devDependencies[name])
+    return Boolean(this.dependencies[name] || this.devDependencies[name]);
   }
 
   dependsAny(...names: string[]) {
-    return names.some((x) => this.depends(x))
+    return names.some(x => this.depends(x));
   }
 
   rootDir(...paths: string[]) {
-    return path.join(path.dirname(this.filename), ...paths)
+    return path.join(path.dirname(this.filename), ...paths);
   }
 }
 
-export default Pkg.createProxy.bind(Pkg)
+export default Pkg.createProxy.bind(Pkg);
--- packages/pkg-proxy/test.ts
+++ packages/pkg-proxy/test.ts
@@ -1,12 +1,12 @@
-import findPkg from './src'
+import findPkg from './src';
 
 test('pkg-proxy', async () => {
-  const pkg = await findPkg()
-  expect(pkg?.name).toBe('admob-plus-monorepo')
+  const pkg = await findPkg();
+  expect(pkg?.name).toBe('admob-plus-monorepo');
 
-  const json = JSON.stringify(pkg)
-  expect(json).toBe(JSON.stringify(pkg?.json))
+  const json = JSON.stringify(pkg);
+  expect(json).toBe(JSON.stringify(pkg?.json));
 
-  Object.assign(pkg, { newprop: 'newprop' })
-  expect(JSON.stringify(pkg)).not.toBe(json)
-})
+  Object.assign(pkg, {newprop: 'newprop'});
+  expect(JSON.stringify(pkg)).not.toBe(json);
+});
