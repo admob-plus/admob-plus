865c8eedc2e7ad831cbd6b984927e387d46d0876
Ratson <github@ratson.name>
2022-06-17 00:54:01 +0800

    test(pkg-proxy): fix type error for undefined pkg

--- packages/pkg-proxy/test.ts
+++ packages/pkg-proxy/test.ts
@@ -1,11 +1,13 @@
+import assert from 'node:assert';
 import findPkg from './src';
 
 test('pkg-proxy', async () => {
   const pkg = await findPkg();
   expect(pkg?.name).toBe('admob-plus-monorepo');
+  assert(pkg);
 
   const json = JSON.stringify(pkg);
-  expect(json).toBe(JSON.stringify(pkg?.json));
+  expect(json).toBe(JSON.stringify(pkg.json));
 
   Object.assign(pkg, {newprop: 'newprop'});
   expect(JSON.stringify(pkg)).not.toBe(json);
