5112db26fd2dfa9475f31c46a6e15d0560b431a3
Ratson <github@ratson.name>
2022-07-25 18:07:41 +0800

    test: config jest to use esm

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,21 @@
-import type {Config} from '@jest/types';
 import {defaults} from 'jest-config';
+import type {InitialOptionsTsJest} from 'ts-jest';
+import {defaultsESM as tsDefaults} from 'ts-jest/presets';
 
-export default async (): Promise<Config.InitialOptions> => ({
+export default async (): Promise<InitialOptionsTsJest> => ({
+  preset: 'ts-jest/presets/default-esm',
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
   transform: {
     // @ts-expect-error wrong type
     ...defaults.transform,
+    ...tsDefaults.transform,
     '^.+\\.tsx?$': 'ts-jest',
   },
   testPathIgnorePatterns: [
@@ -19,5 +30,10 @@ export default async (): Promise<Config.InitialOptions> => ({
   testEnvironmentOptions: {
     url: 'http://localhost/',
   },
-  moduleFileExtensions: ['ts', 'tsx', ...defaults.moduleFileExtensions],
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    ...(tsDefaults.moduleFileExtensions ?? []),
+    ...defaults.moduleFileExtensions,
+  ],
 });
