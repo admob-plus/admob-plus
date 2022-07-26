ae8cb9474601c89031e7657af10c8bd9c8e54cb1
Ratson <github@ratson.name>
2022-07-26 16:55:00 +0800

    feat(ionic): emit cjs main

--- packages/ionic/scripts/build.ts
+++ packages/ionic/scripts/build.ts
@@ -1,30 +1,40 @@
-import del from 'del'
-import execa from 'execa'
-import * as fse from 'fs-extra'
+import del from 'del';
+import execa from 'execa';
+import * as fse from 'fs-extra';
 
 const main = async () => {
-  await del(['build', 'lib', 'ngx', '*.tsbuildinfo'])
+  await del(['build', 'lib', 'ngx', '*.tsbuildinfo']);
 
-  const s = await fse.readFile('./src/ngx/index.ts', 'utf8')
+  const s = await fse.readFile('./src/ngx/index.ts', 'utf8');
   await fse.outputFile(
     './src/lib/index.ts',
     s
       .replace("import { Injectable } from '@angular/core'\n", '')
-      .replace('@Injectable()\n', ''),
-  )
+      .replace('@Injectable()\n', '')
+  );
 
-  await execa('tsc', { stdio: 'inherit' })
   await Promise.all([
-    fse.copy('./build/lib', './lib', { overwrite: true }),
-    fse.copy('./build/ngx', './ngx', { overwrite: true }),
-  ])
+    execa('tsc', {stdio: 'inherit'}),
+    execa('tsc', ['--outDir', 'build/cjs', '-m', 'commonjs'], {
+      stdio: 'inherit',
+    }),
+  ]);
+  await Promise.all([
+    fse.copy('./build/lib', './lib', {overwrite: true}),
+    fse.copy('./build/ngx', './ngx', {overwrite: true}),
+  ]);
+  await Promise.all([
+    fse.copy('./build/cjs/lib', './lib/cjs', {overwrite: true}),
+    fse.copy('./build/cjs/ngx', './ngx/cjs', {overwrite: true}),
+    fse.outputJSON('./ngx/package.json', {
+      ...require('../package.json'),
+      typings: 'index.d.ts',
+      main: './cjs/index.js',
+      module: 'index.js',
+    })
+  ]);
 
-  await fse.outputJSON('./ngx/package.json', {
-    ...require('../package.json'),
-    typings: 'index.d.ts',
-    main: 'index.js',
-    module: 'index.js',
-  })
-}
+  await del(['build']);
+};
 
-main()
+main();
