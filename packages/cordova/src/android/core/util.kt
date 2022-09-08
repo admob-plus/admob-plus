37a5f31a54636c3d7902d45615aadf156363dc5b
Ratson <github@ratson.name>
2022-09-08 09:31:11 +0800

    refactor(cordova): utilize roundToInt()

--- packages/cordova/src/android/core/util.kt
+++ packages/cordova/src/android/core/util.kt
@@ -3,6 +3,7 @@ package admob.plus.core
 import android.content.res.Resources
 import android.util.DisplayMetrics
 import org.json.JSONArray
+import kotlin.math.roundToInt
 
 fun dpToPx(dp: Double): Double {
     return dp * Resources.getSystem().displayMetrics.density
@@ -10,10 +11,10 @@ fun dpToPx(dp: Double): Double {
 
 fun pxToDp(px: Int): Int {
     val displayMetrics = Resources.getSystem().displayMetrics
-    return Math.round(px / (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT))
+    return (px / (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT)).roundToInt()
 }
 
-fun jsonArray2stringList(a: JSONArray?): List<String>? {
+fun jsonArray2stringList(a: JSONArray?): List<String> {
     val result: MutableList<String> = ArrayList()
     if (a == null) {
         return result
