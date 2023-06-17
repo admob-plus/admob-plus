54abaa696157daea936a7e1954de172eb7e3d5ef
Ratson <github@ratson.name>
2023-06-17 09:51:52 +0800

    test(example/ionic-angular): set CHROME_BIN

--- examples/ionic-angular/karma.conf.js
+++ examples/ionic-angular/karma.conf.js
@@ -1,6 +1,10 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+if (!process.env.CHROME_BIN) {
+  process.env.CHROME_BIN = require('puppeteer').executablePath()
+}
+
 module.exports = function (config) {
   config.set({
     basePath: '',
