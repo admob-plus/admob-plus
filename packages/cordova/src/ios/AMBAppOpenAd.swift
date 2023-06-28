2e39dbb4d47fe173c6b279f1af4f1ad2c8e5b67d
Ratson <github@ratson.name>
2023-06-28 23:40:11 +0800

    feat(cordova): remove AppOpenAdOrientation

--- packages/cordova/src/ios/AMBAppOpenAd.swift
+++ packages/cordova/src/ios/AMBAppOpenAd.swift
@@ -3,15 +3,7 @@ import GoogleMobileAds
 
 class AMBAppOpenAd: AMBAdBase, GADFullScreenContentDelegate {
 
-    let orientation: UIInterfaceOrientation
-
     var mAd: GADAppOpenAd?
-
-    init(id: String, adUnitId: String, adRequest: GADRequest, orientation: UIInterfaceOrientation) {
-        self.orientation = orientation
-
-        super.init(id: id, adUnitId: adUnitId, adRequest: adRequest)
-    }
 
     convenience init?(_ ctx: AMBContext) {
         guard let id = ctx.optId(),
@@ -19,14 +11,9 @@ class AMBAppOpenAd: AMBAdBase, GADFullScreenContentDelegate {
         else {
             return nil
         }
-        var orientation = UIInterfaceOrientation.portrait
-        if let v = ctx.opts?.value(forKey: "orientation") as? Int {
-            orientation = UIInterfaceOrientation(rawValue: v)!
-        }
         self.init(id: id,
                   adUnitId: adUnitId,
-                  adRequest: ctx.optGADRequest(),
-                  orientation: orientation)
+                  adRequest: ctx.optGADRequest())
     }
 
     deinit {
@@ -43,7 +30,6 @@ class AMBAppOpenAd: AMBAdBase, GADFullScreenContentDelegate {
         GADAppOpenAd.load(
             withAdUnitID: self.adUnitId,
             request: adRequest,
-            orientation: self.orientation,
             completionHandler: { (ad, error) in
                 if error != nil {
                     self.emit(AMBEvents.adLoadFail, error!)
--- packages/cordova/src/www/ads/app-open.ts
+++ packages/cordova/src/www/ads/app-open.ts
@@ -1,17 +1,7 @@
 import {MobileAd, MobileAdOptions} from './base';
 
-export enum AppOpenAdOrientation {
-  Portrait = 1,
-  PortraitUpsideDown = 2,
-  LandscapeRight = 3,
-  LandscapeLeft = 4,
-}
-
-export class AppOpenAd extends MobileAd<
-  MobileAdOptions & {orientation: AppOpenAdOrientation}
-> {
+export class AppOpenAd extends MobileAd<MobileAdOptions> {
   static readonly cls = 'AppOpenAd';
-  static readonly Orientation = AppOpenAdOrientation;
 
   public isLoaded() {
     return super.isLoaded();
