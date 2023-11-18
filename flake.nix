15c79b910d117f2250bf019c6c213b1a86ccef9b
Ratson <github@ratson.name>
2023-11-18 23:12:27 +0800

    build: avoid circular nx build

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+      perSystem =
+        { config
+        , self'
+        , inputs'
+        , pkgs
+        , system
+        , ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              go-task
+            ] ++ (with nodePackages; [
+              nodejs
+              pnpm
+            ]);
+          };
+        };
+    };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+}
