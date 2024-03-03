e80bb55c0b2186eb3f3fc4ed223151f4113123a5
Ratson <github@ratson.name>
2024-03-03 11:55:03 +0800

    build(nix): replace flake-parts with flakelight

--- flake.nix
+++ flake.nix
@@ -1,42 +1,40 @@
 {
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = inputs.nixpkgs.lib.systems.flakeExposed;
-      perSystem =
-        { config
-        , self'
-        , inputs'
-        , pkgs
-        , system
-        , ...
-        }:
-        {
-          devShells.default = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-              go-task
-              lychee
-            ] ++ [
-              bundletool
-              ffmpeg
-              gst_all_1.gstreamer
-              opencv
-              (pkgs.writeShellScriptBin "bundletool.jar" ''
-                exec bundletool "$@"
-              '')
-            ] ++ (with nodePackages; [
-              nodejs
-              pnpm
-            ]);
+  outputs = inputs@{ flakelight, ... }:
+    flakelight ./. {
+      inherit inputs;
 
-            OPENCV4NODEJS_DISABLE_AUTOBUILD = "1";
-          };
+      devShell = { inputs', pkgs, stdenv, lib, ... }:
+        let
+          pkgs-stable = inputs'.nixpkgs-stable.legacyPackages;
+        in
+        pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            go-task
+            lychee
+          ] ++ [
+            bundletool
+            ffmpeg
+            gst_all_1.gstreamer
+            opencv
+            (writeShellScriptBin "bundletool.jar" ''
+              exec bundletool "$@"
+            '')
+          ] ++ (with nodePackages; [
+            nodejs
+            pnpm
+          ]) ++ lib.optionals stdenv.isDarwin [
+            pkgs-stable.cocoapods
+          ];
+
+          OPENCV4NODEJS_DISABLE_AUTOBUILD = "1";
         };
     };
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/768e0cf423e18cb56eb321f7b73609904d8f1611";
     flake-parts = {
-      url = "github:hercules-ci/flake-parts";
+      url = "github:nix-community/flakelight";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
   };
