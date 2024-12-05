e6bdcd6b1e0f2230d36d9356f79c634cff65098d
Ratson <github@ratson.name>
2024-12-05 12:14:18 +0800

    build: switch to conflake

--- flake.nix
+++ flake.nix
@@ -1,40 +1,57 @@
 {
-  outputs = inputs@{ flakelight, ... }:
-    flakelight ./. {
+  outputs =
+    { conflake, ... }@inputs:
+    conflake ./. {
       inherit inputs;
 
-      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+      ];
 
-      devShell = { inputs', pkgs, stdenv, lib, ... }:
+      devShell =
+        {
+          inputs',
+          pkgs,
+          stdenv,
+          lib,
+          ...
+        }:
         pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            go-task
-            lychee
-          ] ++ [
-            (bundletool.overrideAttrs (attrs: {
-              postInstall = ''
-                ln -s "$src" $out/bin/bundletool.jar
-              '';
-            }))
-            ffmpeg
-            gst_all_1.gstreamer
-            opencv
-          ] ++ (with nodePackages; [
-            nodejs
-            pnpm
-          ]) ++ lib.optionals stdenv.isDarwin [
-            cocoapods
-          ];
+          nativeBuildInputs =
+            with pkgs;
+            [
+              go-task
+              lychee
+            ]
+            ++ [
+              (bundletool.overrideAttrs (attrs: {
+                postInstall = ''
+                  ln -s "$src" $out/bin/bundletool.jar
+                '';
+              }))
+              ffmpeg
+              gst_all_1.gstreamer
+              opencv
+            ]
+            ++ (with nodePackages; [
+              nodejs
+              pnpm
+            ])
+            ++ lib.optionals stdenv.isDarwin [
+              cocoapods
+            ];
 
           OPENCV4NODEJS_DISABLE_AUTOBUILD = "1";
         };
     };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flakelight = {
-      url = "github:nix-community/flakelight";
+    conflake = {
+      url = "github:ratson/conflake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 }
