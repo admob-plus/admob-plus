371258c22082e4f932904fb2cc5a0a3cbf18f1f6
Ratson <github@ratson.name>
2025-03-31 21:08:54 +0800

    build(nix): install Android SDK

--- flake.nix
+++ flake.nix
@@ -10,22 +10,50 @@
         "x86_64-darwin"
       ];
 
+      nixpkgs.config = {
+        android_sdk.accept_license = true;
+        allowUnfree = true;
+      };
+
       devShell =
         {
           inputs',
           pkgs,
           stdenv,
           lib,
+          androidenv,
           ...
         }:
+        let
+          buildToolsVersion = "35.0.1";
+          androidComposition = androidenv.composeAndroidPackages {
+            buildToolsVersions = [ buildToolsVersion ];
+            platformVersions = [ "35" ];
+            includeEmulator = true;
+            includeSources = false;
+            includeSystemImages = true;
+            systemImageTypes = [ "google_apis" ];
+            abiVersions = [ "x86_64" ];
+            useGoogleAPIs = false;
+            includeExtras = [ "extras;google;gcm" ];
+          };
+          androidSdk = androidComposition.androidsdk;
+          jdk = pkgs.openjdk17_headless;
+
+          ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
+        in
         pkgs.mkShell {
-          nativeBuildInputs =
+          inherit ANDROID_HOME;
+
+          buildInputs =
             with pkgs;
             [
               go-task
               lychee
             ]
             ++ [
+              androidSdk
+              jdk
               (bundletool.overrideAttrs (attrs: {
                 postInstall = ''
                   ln -s "$src" $out/bin/bundletool.jar
@@ -43,7 +71,13 @@
               cocoapods
             ];
 
+          ANDROID_JAVA_HOME = "${jdk.home}";
+          ANDROID_SDK_ROOT = "${ANDROID_HOME}";
+          ANDROID_NDK_ROOT = "${ANDROID_HOME}/ndk-bundle";
+          GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_HOME}/build-tools/${buildToolsVersion}/aapt2";
+
           OPENCV4NODEJS_DISABLE_AUTOBUILD = "1";
+          QT_QPA_PLATFORM = "wayland;xcb";
         };
     };
 
